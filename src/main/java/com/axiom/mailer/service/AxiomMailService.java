8b751c84715fa0816eaec3e78461c15605c95936
Mohammed Nebin <nebinmohammed.sa.007@gmail.com>
2025-12-10 11:18:34 +0400

    Nebin  - New Changes

--- src/main/java/com/axiom/mailer/service/AxiomMailService.java
+++ src/main/java/com/axiom/mailer/service/AxiomMailService.java
@@ -1,6 +1,12 @@
 package com.axiom.mailer.service;
 
 import java.io.File;
+import java.util.Hashtable;
+
+import javax.naming.directory.Attribute;
+import javax.naming.directory.Attributes;
+import javax.naming.directory.DirContext;
+import javax.naming.directory.InitialDirContext;
 
 import org.springframework.context.annotation.Lazy;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -17,53 +23,66 @@ import jakarta.mail.internet.MimeMessage;
 public class AxiomMailService {
 
 	private final JavaMailSender mailSender;
-    private final AxiomMailProperties props;
+	private final AxiomMailProperties props;
 
-    public AxiomMailService(@Lazy JavaMailSender mailSender, @Lazy AxiomMailProperties props) {
-        this.mailSender = mailSender;
-        this.props = props;
-    }
+	public AxiomMailService(@Lazy JavaMailSender mailSender, @Lazy AxiomMailProperties props) {
+		this.mailSender = mailSender;
+		this.props = props;
+	}
 
-    public String sendMail(AxiomMailRequest request) {
-        try {
-            MimeMessage mimeMessage = mailSender.createMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+	public boolean hasMXRecord(String domain) {
+		try {
+			domain = domain.substring(domain.indexOf("@") + 1);
+			Hashtable<String, String> env = new Hashtable<>();
+			env.put("java.naming.factory.initial", "com.sun.jndi.dns.DnsContextFactory");
 
-            String fromFullName = String.format("%s <%s>", props.getFromName(), props.getFromEmail());
+			DirContext ctx = new InitialDirContext(env);
+			Attributes attrs = ctx.getAttributes(domain, new String[] { "MX" });
+			Attribute attr = attrs.get("MX");
 
-            helper.setFrom(fromFullName);
-            helper.setTo(request.to());
-            helper.setSubject(replacePlaceholder(AxiomMailConstants.COMPANY_PLACEHOLDER,request.subject(), request.companyName()) );
+			return attr != null;
+		} catch (Exception e) {
+			return false;
+		}
+	}
 
-            // Replace placeholder here
-            String finalHtml = replacePlaceholder(AxiomMailConstants.COMPANY_PLACEHOLDER,request.htmlBody(), request.companyName());
-            finalHtml = replacePlaceholder(AxiomMailConstants.PERSONALIZATION_PLACEHOLDER,finalHtml, request.personalization());
-            finalHtml = finalHtml
-                    .replace("\r\n", "<br>")
-                    .replace("\n", "<br>");
-            helper.setText(finalHtml, true);
-            File folder = new File(props.getAttachmentsFolder());
-            if (folder.exists() && folder.isDirectory()) {
-                File[] files = folder.listFiles();
-                if (files != null) {
-                    for (File file : files) {
-                        helper.addAttachment(file.getName(), file);
-                    }
-                }
-            }
+	public String sendMail(AxiomMailRequest request) {
+		try {
+			if (!hasMXRecord(request.to())) {
+				return "Mail not sent because mail domain is not found";
+			}
+			MimeMessage mimeMessage = mailSender.createMimeMessage();
+			MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+			String fromFullName = String.format("%s <%s>", props.getFromName(), props.getFromEmail());
+			helper.setFrom(fromFullName);
+			helper.setTo(request.to());
+			helper.setSubject(replacePlaceholder(AxiomMailConstants.COMPANY_PLACEHOLDER, request.subject(),
+					request.companyName()));
+			String finalHtml = replacePlaceholder(AxiomMailConstants.COMPANY_PLACEHOLDER, request.htmlBody(),
+					request.companyName());
+			finalHtml = replacePlaceholder(AxiomMailConstants.PERSONALIZATION_PLACEHOLDER, finalHtml,
+					request.personalization());
+			finalHtml = finalHtml.replace("\r\n", "<br>").replace("\n", "<br>");
+			helper.setText(finalHtml, true);
+			File folder = new File(props.getAttachmentsFolder());
+			if (folder.exists() && folder.isDirectory()) {
+				File[] files = folder.listFiles();
+				if (files != null) {
+					for (File file : files) {
+						helper.addAttachment(file.getName(), file);
+					}
+				}
+			}
+			mailSender.send(mimeMessage);
+			return "Mail sent successfully";
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "Failed: " + e.getMessage();
+		}
+	}
 
-            mailSender.send(mimeMessage);
-            return "Mail sent successfully";
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            return "Failed: " + e.getMessage();
-        }
-    }
-    
-    private String replacePlaceholder(String placeHolder, String htmlBody, String companyName) {
-        return htmlBody.replace(placeHolder, companyName);
-    }
-
+	private String replacePlaceholder(String placeHolder, String htmlBody, String companyName) {
+		return htmlBody.replace(placeHolder, companyName);
+	}
 
 }
